037bd704f35e2de0645a2798d0078816953696bd
Sebas Dev <sebasariasdev@gmail.com>
2023-06-26 18:39:55 +0800

    First Journal Views

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,13 +1,27 @@
-
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Non veniam dolor minim proident minim deserunt eu irure deserunt aute.
-      </Typography>
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          right: 50,
+          bottom: 50,
+          position: 'fixed',
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
